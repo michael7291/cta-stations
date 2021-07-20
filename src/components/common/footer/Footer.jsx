1ee07b6e03f785c6aebcdb9ec16e62d6a0fcf3ce
michael7291 <35711500+michael7291@users.noreply.github.com>
2021-07-19 23:16:21 -0700

    Add files via upload

---
+++ src/components/common/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer () {
+
+  return (
+    <section className="footer">
+      <hr className="footer-seperator" />
+      <section className="footer-info">
+        <section className="footer-info-left">       
+        </section>
+        <section className="footer-info-center">
+          <section className="footer-info__terms">
+            <br />
+            Copyright © 
+          </section>
+        </section>
+      </section>
+      <hr className="footer-seperator" />
+    </section>
+  )
+
+}
+
+export default Footer;
